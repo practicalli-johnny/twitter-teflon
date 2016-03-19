a3da1d340523e33db882fd719a3ee2bb0403012d
Sam Halliday <sam.halliday@gmail.com>
2016-03-19 20:08:41 +0000

    twitter specific requests

--- src/fommil/teflon/core.clj
+++ src/fommil/teflon/core.clj
@@ -21,6 +21,28 @@
                   "https://api.twitter.com/1.1/statuses/user_timeline.json"
                   {:screen_name "fommil"}))
 
+(defn twitter-request
+  "Wraps `http/request' with OAuth1.0 authentication assuming that the consumer and access tokens are defined."
+  [method url query-params]
+  (let [consumer (oauth/make-consumer
+                  +twitter_consumer_key+
+                  +twitter_consumer_secret+
+                  "https://api.twitter.com/oauth/request_token"
+                  "https://api.twitter.com/oauth/access_token"
+                  "https://api.twitter.com/oauth/authorize"
+                  :hmac-sha1)
+        credentials (oauth/credentials
+                     consumer
+                     +twitter_access_token+
+                     +twitter_access_token_secret+
+                     method
+                     url
+                     query-params)]
+    
+    )
+  )
+
+
 (defn -main []
   (println "Hello, World!"
            (http/request
