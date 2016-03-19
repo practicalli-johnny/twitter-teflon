7ac580f5aa99789ea5cdd47ccc6313ca505791e3
Sam Halliday <sam.halliday@gmail.com>
2016-03-19 12:36:02 +0000

    change to LGPL and add core.async

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
 (defproject twitter-teflon "0.1.0-SNAPSHOT"
   :description "Delete all your low-score tweets"
   :url "https://github.com/fommil/twitter-teflon"
-  :license {:name "Mozilla Public License 2.0"
-            :url "https://www.mozilla.org/en-US/MPL/2.0/"}
+  :license {:name "GNU Lesser General Public License"
+            :url "https://www.gnu.org/copyleft/lesser.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/core.async "0.2.374"]
                  [http-kit "2.1.18"]
                  [cheshire "5.5.0"]]
   :main fommil.teflon.core)
