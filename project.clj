9b7077b693cb5aea265fe0774bd2c23219d6ea7f
Sam Halliday <sam.halliday@gmail.com>
2016-03-19 10:44:59 +0000

    initial project structure

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject twitter-teflon "0.1.0-SNAPSHOT"
+  :description "Delete all your low-score tweets"
+  :url "https://github.com/fommil/twitter-teflon"
+  :license {:name "Mozilla Public License 2.0"
+            :url "https://www.mozilla.org/en-US/MPL/2.0/"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
