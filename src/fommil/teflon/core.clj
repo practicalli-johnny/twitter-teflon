2645cba389a4e9884db059d8fd68c5e31035d743
Sam Halliday <sam.halliday@gmail.com>
2016-03-19 18:47:47 +0000

    oauth works

--- src/fommil/teflon/core.clj
+++ src/fommil/teflon/core.clj
@@ -1,14 +1,9 @@
 (ns fommil.teflon.core
   (:require
    [oauth.client :as oauth]
-   ;;[oauth.twitter :as twitter]
    [clj-http.client :as http]))
 
 (load-file "config.clj")
-
-;; (def request-token (twitter/oauth-request-token
-;;                     +twitter_consumer_key+
-;;                     +twitter_consumer_secret+))
 
 (def consumer (oauth/make-consumer
                +twitter_consumer_key+
@@ -22,22 +17,17 @@
                   consumer
                   +twitter_access_token+
                   +twitter_access_token_secret+
-                  :GET
+                  :get
                   "https://api.twitter.com/1.1/statuses/user_timeline.json"
                   {:screen_name "fommil"}))
 
-
-(oauth/access-token consumer (oauth/request-token consumer))
-
-(http/request
- {:method :get
-  :url "https://api.twitter.com/1.1/statuses/user_timeline.json"
-  :query-params (merge credentials {:screen_name "fommil"})
-  })
-
-
 (defn -main []
   (println "Hello, World!"
+           (http/request
+            {:method :get
+             :url "https://api.twitter.com/1.1/statuses/user_timeline.json"
+             :query-params (merge credentials {:screen_name "fommil"})
+             })
            ))
 
 
