ee0dff47c476e239171576fae8944f5bda7c1cd8
Sam Halliday <sam.halliday@gmail.com>
2016-03-26 23:16:15 +0000

    channel tests

--- test/fommil/channels_test.clj
+++ test/fommil/channels_test.clj
@@ -5,7 +5,7 @@
 ;; happens, or we get a slow machine, things might fail.
 (ns fommil.channels-test
   (:use
-   [clojure.core.async :as async :only [<!! >! go-loop]])
+   [clojure.core.async :as async :only [<!! >! go go-loop]])
   (:require
    [midje.sweet :refer :all]
    [fommil.channels :refer :all]))
@@ -20,33 +20,40 @@
 
 (fact "heartbeats can have an initial delay"
       (timed (<!! (heartbeat 50)))
-      => (just {:result :ping :time #(<= 0 % 10)})
+      => (just {:result :ping :time #(<= 0 % 20)})
       (timed (<!! (heartbeat 50 0)))
-      => (just {:result :ping :time #(<= 0 % 10)})
+      => (just {:result :ping :time #(<= 0 % 20)})
       (timed (<!! (heartbeat 50 10)))
       => (just {:result :ping :time #(<= 5 % 20)})
       (timed (<!! (heartbeat 50 50)))
-      => (just {:result :ping :time #(<= 50 % 60)})
- )
+      => (just {:result :ping :time #(<= 50 % 60)}))
 
 (fact "heartbeats arrive on time (on average)"
       (timed (<!! (async/partition 5 (heartbeat 50))))
       => (just {:result [:ping :ping :ping :ping :ping]
-                :time #(<= 190 % 210)})
-      )
+                :time #(<= 190 % 210)}))
 
 (fact "channels can be rate limited"
-      ;; FIXME there is a bug in here somewhere...
       (let [counter (atom 0)
             in      (async/chan)
             limited (rate-limited 10 in)
             start   (System/currentTimeMillis)]
         (go-loop []
-          (dosync (alter counter inc))
+          (dosync (swap! counter inc))
           (>! in :foo)
           (recur))
         (while (< (- (System/currentTimeMillis) start) 100)
           (<!! limited))
-        (@counter))
-      => (just #(<= 5 % 15))
-      )
+        @counter)
+      => #(<= 5 % 15))
+
+(fact "rate-limited channels are closed correctly"
+      (let [in (async/chan)
+            limited (rate-limited 10 in)]
+        (go []
+            (>! in :foo)
+            (async/close! in)
+            (>! in :foo))
+        (<!! limited)
+        (<!! limited))
+      => nil)
